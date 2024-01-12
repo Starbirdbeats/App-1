ba207d01cfed1bf7820c00de8201f29cee05e975
Starbird <marcellohaupt@gmail.com>
2024-01-12 08:06:51 +0200

    handled errors for edit and complete buttons and added clock

--- gui.py
+++ gui.py
@@ -1,35 +1,44 @@
 import functions
 import PySimpleGUI as sg
+from time import strftime
 
-layout = [[sg.Text("Create a task:")],
-          [sg.InputText(tooltip="Enter a task", key="task"), sg.Button("Add")],
+layout = [[sg.Text("", key="clock")],
+          [sg.Text("Create a task:")],
+          [sg.InputText(tooltip="Enter a task", key="task", size=47), sg.Button("Add")],
           [sg.Listbox(values=functions.read_tasks(), key="tasks", enable_events=True, size=[45, 10]), sg.Button("Edit"), sg.Button("complete")],
           [sg.Button("exit")]]
 
 window_main = sg.Window("Simple To-Do App", layout=layout, font=("Montserrat", 8))
 
 while True:
-    event, values = window_main.read()
-    print(f"{event}\n{values}")
+    event, values = window_main.read(timeout=200)
+    window_main["clock"].update(strftime("%b %d, %Y %H:%M:%S"))
     match event:
         case "Add":
             tasks = functions.read_tasks()
-            tasks.append(values["task"])
+            tasks.append(f"{values['task']}\n")
             functions.create_tasks(tasks)
+            print(tasks)
             window_main["tasks"].update(values=tasks)
         case "Edit":
-            task = values["tasks"][0]
-            tasks = functions.read_tasks()
-            index = tasks.index(task)
-            tasks[index] = values["task"]
-            functions.create_tasks(tasks)
-            window_main["tasks"].update(values=tasks)
+            try:
+                task = values["tasks"][0]
+                tasks = functions.read_tasks()
+                index = tasks.index(task)
+                tasks[index] = values["task"]
+                functions.create_tasks(tasks)
+                window_main["tasks"].update(values=tasks)
+            except IndexError:
+                sg.popup("First select a task, before pressing edit.", font=("Montserrat", 8))
         case "complete":
-            tasks = functions.read_tasks()
-            tasks.remove(values["tasks"][0])
-            functions.create_tasks(tasks)
-            window_main["tasks"].update(values=tasks)
-            window_main["task"].update(value="")
+            try:
+                tasks = functions.read_tasks()
+                tasks.remove(values["tasks"][0])
+                functions.create_tasks(tasks)
+                window_main["tasks"].update(values=tasks)
+                window_main["task"].update(value="")
+            except IndexError:
+                sg.popup("First select a task, before pressing complete.", font=("Montserrat", 8))
         case "exit":
             break
         case "tasks":
